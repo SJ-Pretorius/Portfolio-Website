dbe010cdba1764d725435e79225c94d9b81b573e
SJ Pretorius <sjpret12345@gmail.com>
2025-07-19 19:48:17 +0200

    Consistency fix

--- cms/backend/modules/header.php
+++ cms/backend/modules/header.php
@@ -1,7 +1,7 @@
 <?php
     $GLOBALS['smarty']->assign("STYLES", array(
-            "frontend/assets/css/core.css",
-            "frontend/assets/css/vendor_bundle.css",
+            "/cms/frontend/assets/css/core.css",
+            "/cms/frontend/assets/css/vendor_bundle.css",
         ));
             
     $GLOBALS['smarty']->display("header.tpl");
